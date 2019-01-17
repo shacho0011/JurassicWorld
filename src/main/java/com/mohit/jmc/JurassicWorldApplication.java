44ff7bb9642d0a7f9e23e659d8aa7636ee828c0d
Shadhin <shadhin.aust@gmail.com>
2019-01-17 23:24:26 +0600

    init commit: jurassic world basic structure

---
+++ src/main/java/com/mohit/jmc/JurassicWorldApplication.java
@@ -0,0 +1,14 @@
+package com.mohit.jmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JurassicWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JurassicWorldApplication.class, args);
+	}
+
+}
+
