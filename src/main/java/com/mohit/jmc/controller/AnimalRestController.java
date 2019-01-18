b9628687482accf0023e4bcf21718d4a1307d592
Shadhin <shadhin.aust@gmail.com>
2019-01-19 02:29:11 +0600

    added rest ctrl: animal get api

---
+++ src/main/java/com/mohit/jmc/controller/AnimalRestController.java
@@ -0,0 +1,82 @@
+package com.mohit.jmc.controller;
+
+import com.mohit.jmc.dto.AnimalOverviewDto;
+import com.mohit.jmc.model.Animal;
+import com.mohit.jmc.service.AnimalService;
+import com.mohit.jmc.service.DtoUtilService;
+import jdk.nashorn.internal.runtime.options.Option;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class AnimalRestController {
+
+    @Autowired
+    AnimalService animalService;
+    @Autowired
+    DtoUtilService dtoUtilService;
+
+    @GetMapping("/animals")
+    ResponseEntity<Object> getAllAnimal(@RequestHeader Optional<Long> id){
+        ResponseEntity<Object> responseEntity = null;
+
+        try {
+
+            if(id.isPresent()){
+                Animal animal = animalService.getAnimalById(id.get());
+                AnimalOverviewDto animalOverviewDto = dtoUtilService.convertToAnimalOverviewDto(animal);
+                responseEntity = new ResponseEntity<>(animalOverviewDto, null, HttpStatus.OK);
+            }else{
+                List<Animal> animalList = animalService.getAllAnimal();
+                if(animalList.size() > 0){
+                    List<AnimalOverviewDto> animalOverviewDtoList = new ArrayList<>();
+                    animalList.forEach(animal -> {
+                        animalOverviewDtoList.add(dtoUtilService.convertToAnimalOverviewDto(animal));
+                    });
+                    responseEntity = new ResponseEntity<>(animalOverviewDtoList, null, HttpStatus.OK);
+                }else{
+                    responseEntity = new ResponseEntity<>("No animal available!", null, HttpStatus.OK);
+                }
+            }
+
+        }catch (Exception e){
+            responseEntity = new ResponseEntity<>("Internal server error!", null, HttpStatus.INTERNAL_SERVER_ERROR);
+            e.printStackTrace();
+        }
+
+        return  responseEntity;
+    }
+
+    @GetMapping("/animal")
+    ResponseEntity<Object> getAnimalsByName(@RequestHeader String name){
+        ResponseEntity<Object> responseEntity = null;
+
+        try {
+
+            List<Animal> animalList = animalService.getAnimalByName(name);
+            if(animalList.size() > 0){
+                List<AnimalOverviewDto> animalOverviewDtoList = new ArrayList<>();
+                animalList.forEach(animal -> {
+                    animalOverviewDtoList.add(dtoUtilService.convertToAnimalOverviewDto(animal));
+                });
+                responseEntity = new ResponseEntity<>(animalOverviewDtoList, null, HttpStatus.OK);
+            }else{
+                responseEntity = new ResponseEntity<>("No animal available!", null, HttpStatus.OK);
+            }
+
+        }catch (Exception e){
+            responseEntity = new ResponseEntity<>("Internal server error!", null, HttpStatus.INTERNAL_SERVER_ERROR);
+            e.printStackTrace();
+        }
+
+        return  responseEntity;
+    }
+
+}
