ef85535988ad60474054cde0d317d26a6647da4c
Shadhin <shadhin.aust@gmail.com>
2019-01-19 01:47:12 +0600

    updated repository: animal

--- src/main/java/com/mohit/jmc/repository/AnimalRepository.java
+++ src/main/java/com/mohit/jmc/repository/AnimalRepository.java
@@ -4,7 +4,11 @@ import com.mohit.jmc.model.Animal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
+
+    List<Animal> findByNameContaining(String name);
 
 }
