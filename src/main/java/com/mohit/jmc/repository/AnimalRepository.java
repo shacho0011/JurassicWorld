9d42ef7f4cc975e96372c9b7f7b3052b9fb86c77
Shadhin <shadhin.aust@gmail.com>
2019-01-19 01:06:42 +0600

    added repository: user, animal

---
+++ src/main/java/com/mohit/jmc/repository/AnimalRepository.java
@@ -0,0 +1,10 @@
+package com.mohit.jmc.repository;
+
+import com.mohit.jmc.model.Animal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnimalRepository extends JpaRepository<Animal, Long> {
+
+}
