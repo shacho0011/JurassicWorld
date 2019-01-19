3023f8da10e276fbdab9bd75a7424bc6342d483e
shadhin <shadhin@shadhin-HP-ProBook-440-G4>
2019-01-19 16:24:28 +0600

    updated user ctrl: added create user api

--- src/main/java/com/mohit/jmc/controller/UserRestController.java
+++ src/main/java/com/mohit/jmc/controller/UserRestController.java
@@ -1,54 +1,85 @@
 package com.mohit.jmc.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mohit.jmc.dto.UserOverviewDto;
+import com.mohit.jmc.dto.UserLoginDto;
+import com.mohit.jmc.dto.UserRegDto;
 import com.mohit.jmc.model.User;
 import com.mohit.jmc.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/user")
 public class UserRestController {
 
-    @Autowired
-    UserService userService;
-    @Autowired
-    PasswordEncoder passwordEncoder;
+	@Autowired
+	UserService userService;
+	@Autowired
+	PasswordEncoder passwordEncoder;
 
-    @GetMapping("/login")
-    ResponseEntity<Object> getUserByUsername(@RequestBody String requestData){
-        ResponseEntity<Object> responseEntity = null;
+	@GetMapping("/login")
+	ResponseEntity<Object> getUserByUsername(@RequestBody String requestData) {
+		ResponseEntity<Object> responseEntity = null;
 
-        try {
-            UserOverviewDto userOverviewDto = new ObjectMapper().readValue(requestData, UserOverviewDto.class);
-            User user = null;
-            if(userOverviewDto.getEmail() != null && userOverviewDto.getPassword() != null){
-                user = userService.getUserByUsername(userOverviewDto.getEmail());
-                if(user != null){
-                    if(passwordEncoder.matches(userOverviewDto.getPassword(), user.getPassword())){
-                        responseEntity = new ResponseEntity<>("Success", null, HttpStatus.OK);
-                    }else {
-                        responseEntity = new ResponseEntity<>("Password mismatch!", null, HttpStatus.NOT_FOUND);
-                    }
-                }else{
-                    responseEntity = new ResponseEntity<>("User not found!", null, HttpStatus.NOT_FOUND);
-                }
+		try {
 
-            }else {
-                responseEntity = new ResponseEntity<>("Request error", null, HttpStatus.BAD_REQUEST);
-            }
+			UserLoginDto userLoginDto = new ObjectMapper().readValue(requestData, UserLoginDto.class);
+			User user = null;
+			if (userLoginDto.getEmail() != null && userLoginDto.getPassword() != null) {
+				user = userService.getUserByUsername(userLoginDto.getEmail());
+				if (user != null) {
+					if (passwordEncoder.matches(userLoginDto.getPassword(), user.getPassword())) {
+						responseEntity = new ResponseEntity<>("Success", null, HttpStatus.OK);
+					} else {
+						responseEntity = new ResponseEntity<>("Password mismatch!", null, HttpStatus.NOT_FOUND);
+					}
+				} else {
+					responseEntity = new ResponseEntity<>("User not found!", null, HttpStatus.NOT_FOUND);
+				}
+			} else {
+				responseEntity = new ResponseEntity<>("Request error", null, HttpStatus.BAD_REQUEST);
+			}
 
-        }catch (Exception e){
-            responseEntity = new ResponseEntity<>("Internal server error!", null, HttpStatus.INTERNAL_SERVER_ERROR);
-            e.printStackTrace();
-        }
+		} catch (Exception e) {
 
+			responseEntity = new ResponseEntity<>("Internal server error!", null, HttpStatus.INTERNAL_SERVER_ERROR);
+			e.printStackTrace();
 
-        return responseEntity;
-    }
+		}
+
+		return responseEntity;
+	}
+
+	@PostMapping("/register")
+	public ResponseEntity<Object> userRegistration(@RequestBody String requestData) {
+		ResponseEntity<Object> responseEntity = null;
+
+		try {
+
+			UserRegDto userRegDto = new ObjectMapper().readValue(requestData, UserRegDto.class);
+			User user = null;
+			user = userService.getUserByUsername(userRegDto.getEmail());
+			if (user != null) {
+				responseEntity = new ResponseEntity<>("Already registered", null, HttpStatus.INTERNAL_SERVER_ERROR);
+			} else {
+				user = new User();
+				user = userService.createOrUpdateUser(user, userRegDto);
+				responseEntity = new ResponseEntity<>("Insert operation success", null,
+						HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+
+		} catch (Exception e) {
+
+			responseEntity = new ResponseEntity<>("Internal server error!", null, HttpStatus.INTERNAL_SERVER_ERROR);
+			e.printStackTrace();
+
+		}
+
+		return responseEntity;
+	}
+
 }
