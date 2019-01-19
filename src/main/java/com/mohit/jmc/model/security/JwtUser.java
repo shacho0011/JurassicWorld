4e024b376e0cb8d7af4dc92eca402071cab577be
shadhin <shadhin@shadhin-HP-ProBook-440-G4>
2019-01-20 01:15:50 +0600

    updated model: fixed jwt models

---
+++ src/main/java/com/mohit/jmc/model/security/JwtUser.java
@@ -0,0 +1,24 @@
+package com.mohit.jmc.model.security;
+
+public class JwtUser {
+
+	String username;
+	String role;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+}
---
+++ src/main/java/com/mohit/jmc/model/security/JwtUserDetails.java
@@ -0,0 +1,61 @@
+package com.mohit.jmc.model.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class JwtUserDetails implements UserDetails {
+
+	String username;
+	String token;
+	Collection<? extends GrantedAuthority> authorities;
+
+	public JwtUserDetails(String useruame, String token, List<GrantedAuthority> grantedAuthorities) {
+		this.username = username;
+		this.token = token;
+		this.authorities = grantedAuthorities;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+
+}
