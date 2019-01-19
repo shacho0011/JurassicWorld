809aeb551bfce7990a09985b5586959d502bd7af
shadhin <shadhin@shadhin-HP-ProBook-440-G4>
2019-01-19 18:30:41 +0600

    added security token: jwt token impl

---
+++ src/main/java/com/mohit/jmc/security/JwtGenerator.java
@@ -0,0 +1,34 @@
+package com.mohit.jmc.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.mohit.jmc.dto.security.JwtUser;
+
+@Component
+public class JwtGenerator {
+
+	@Value("${jwt.secret}")
+	private String token;
+
+	public String generate(JwtUser jwtUser) {
+
+		Claims claims = Jwts.claims().setSubject(jwtUser.getUserName());
+		claims.put("userId", String.valueOf(jwtUser.getId()));
+		claims.put("role", jwtUser.getRole());
+
+		long nowMillis = System.currentTimeMillis();
+		Date now = new Date(nowMillis);
+		long expMillis = nowMillis + 60000;
+		Date exp = new Date(expMillis);
+
+		return Jwts.builder().setIssuedAt(now).setClaims(claims).signWith(SignatureAlgorithm.HS512, token)
+				.setExpiration(exp).compact();
+	}
+}
