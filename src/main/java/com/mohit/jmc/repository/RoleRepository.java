3a3240c38b5ec8f5ca60d5ef60ee13b746a3004c
Shadhin <shadhin.aust@gmail.com>
2019-01-19 09:57:07 +0600

    added repository: role repo

---
+++ src/main/java/com/mohit/jmc/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.mohit.jmc.repository;
+
+import com.mohit.jmc.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
